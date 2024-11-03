7eb36674e0fca29975854e4ecfdaa39b1b2ba1c7
Andrea Ciceri <andrea.ciceri@autistici.org>
2024-11-03 10:20:37 +0100

    Explicit tapping term

--- keyboards/bluebell/swoop/keymaps/aciceri_miryoku/keymap.c
+++ keyboards/bluebell/swoop/keymaps/aciceri_miryoku/keymap.c
@@ -13,6 +13,9 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+
+#define TAPPING_TERM 200
+
 #include QMK_KEYBOARD_H
 
 enum layers {
