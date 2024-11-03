0170a120b9395739020253a696e12e7eca9f20fb
Andrea Ciceri <andrea.ciceri@autistici.org>
2024-11-03 10:20:57 +0100

    Swap space and tab

--- keyboards/bluebell/swoop/keymaps/aciceri_miryoku/keymap.c
+++ keyboards/bluebell/swoop/keymaps/aciceri_miryoku/keymap.c
@@ -35,7 +35,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_Q,              KC_W,              KC_E,              KC_R,              KC_T,              KC_Y,              KC_U,              KC_I,              KC_O,              KC_P,
   LGUI_T(KC_A),      LALT_T(KC_S),      LCTL_T(KC_D),      LSFT_T(KC_F),      KC_G,              KC_H,              RSFT_T(KC_J),      RCTL_T(KC_K),      RALT_T(KC_L),      RGUI_T(KC_QUOT),
   LT(_BUTTON,KC_Z),  ALGR_T(KC_X),      KC_C,              KC_V,              KC_B,              KC_N,              KC_M,              KC_COMM,           ALGR_T(KC_DOT),    LT(_BUTTON,KC_SLSH),
-                                        LT(_MEDIA,KC_ESC), LT(_NAV,KC_SPC),   LT(_MOUSE,KC_TAB), LT(_SYM,KC_ENT),   LT(_NUM,KC_BSPC),  LT(_FUN,KC_DEL)
+                                        LT(_MEDIA,KC_ESC), LT(_NAV,KC_TAB),   LT(_MOUSE,KC_SPC), LT(_SYM,KC_ENT),   LT(_NUM,KC_BSPC),  LT(_FUN,KC_DEL)
 ),
 
 [_NAV] = LAYOUT_split_3x5_3(
